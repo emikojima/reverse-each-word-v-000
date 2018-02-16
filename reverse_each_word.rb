41784d283b8a415a8be1d366109a3b3f00a6b5ec
emi kojima <emi8emi@gmail.com>
2018-02-16 00:18:42 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
-def reverse_each_word(words)
+def reverse_each_word(array)
   new_array = array.split(" ")
   reversed_array = new_array.each {|x| x.reverse!}
   return reversed_array.join(" ")
-    
 end
      
